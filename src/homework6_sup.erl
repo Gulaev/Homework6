7d5c5414d91842cc36c4d44b468165000022b765
Denis Gulaev <dennysittohero@gmail.com>
2024-12-12 09:09:15 +0200

    Uploaad working version

--- src/homework6_sup.erl
+++ src/homework6_sup.erl
@@ -4,20 +4,22 @@
 %%%-------------------------------------------------------------------
 
 -module(homework6_sup).
-
 -behaviour(supervisor).
 
 -export([start_link/0]).
 -export([init/1]).
 
--define(SERVER, ?MODULE).
-
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    {ok, {{one_for_one, 5, 10},
-        [
-            {homework6_cache, {homework6_cache, start_link, []},
-                permanent, 5000, worker, [homework6_cache]}
-        ]}}.
+    SupFlags = #{strategy => one_for_one, intensity => 1, period => 5},
+    ChildSpecs = [
+        #{id => homework6_server,
+            start => {homework6_server, start_link, []},
+            restart => permanent,
+            shutdown => 5000,
+            type => worker,
+            modules => [homework6_server]}
+    ],
+    {ok, {SupFlags, ChildSpecs}}.
--- test/homework6_ct_SUITE.erl
+++ test/homework6_ct_SUITE.erl
@@ -6,47 +6,50 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(homework6_ct_SUITE).
--author("denisgulaev").
 -include_lib("common_test/include/ct.hrl").
--include_lib("eunit/include/eunit.hrl").
 
-%% API
--export([
-  all/0,
-  init_per_suite/1,
-  end_per_suite/1,
-  init_per_testcase/2,
-  end_per_testcase/2,
-  test_insert_lookup/1
-]).
+-export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2, end_per_testcase/2, create_table_test/1, insert_test/1, lookup_test/1]).
 
-all() -> [test_insert_lookup].
+all() -> [create_table_test, insert_test, lookup_test].
 
 init_per_suite(Config) ->
-  io:format("Starting suite...~n", []),
-  application:start(homework6),
-  homework6_cache:start_link(),
+  ok = application:start(homework6),
   Config.
 
-end_per_suite(Config) ->
-  io:format("Ending suite...~n", []),
-  application:stop(homework6),
-  Config.
+end_per_suite(_Config) ->
+  application:stop(homework6).
 
 init_per_testcase(_TestCase, Config) ->
-  io:format("Starting test case...~n", []),
   Config.
 
 end_per_testcase(_TestCase, _Config) ->
-  io:format("Ending test case...~n", []),
   ok.
 
-test_insert_lookup(_Config) ->
-  io:format("Running test_insert_lookup...~n", []),
-  ok = homework6_cache:create(test_cache),
-  ok = homework6_cache:insert(test_cache, key1, value1, 60),
-  Value = homework6_cache:lookup(test_cache, key1),
-  ?assertEqual(value1, Value),
-  timer:sleep(61000),
-  UndefinedValue = homework6_cache:lookup(test_cache, key1),
-  ?assertEqual(undefined, UndefinedValue).
+create_table_test(_Config) ->
+  ok = homework6_server:create(test_table),
+  Info = ets:info(test_table),
+  case Info of
+    undefined -> erlang:error({test_failed, "Table test_table was not created"});
+    _ -> ok
+  end.
+
+insert_test(_Config) ->
+  ok = homework6_server:insert(test_table, <<"key1">>, <<"value1">>),
+  Result = ets:lookup(test_table, <<"key1">>),
+  case Result of
+    [{<<"key1">>, <<"value1">>, undefined}] -> ok;
+    _ ->
+      ErrorMsg = io_lib:format("Key ~p was not inserted correctly", [<<"key1">>]),
+      erlang:error({test_failed, ErrorMsg})
+  end.
+
+lookup_test(_Config) ->
+  ok = homework6_server:insert(test_table, <<"key2">>, <<"value2">>, 5),
+  timer:sleep(6000),
+  Result = homework6_server:lookup(test_table, <<"key2">>),
+  case Result of
+    undefined -> ok;
+    _ ->
+      ErrorMsg = io_lib:format("Key ~p did not expire as expected", [<<"key2">>]),
+      erlang:error({test_failed, ErrorMsg})
+  end.
